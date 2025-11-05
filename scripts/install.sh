81b4ed5dcc2556e3603cb91c5580d8e54d35e286
Zamderax <zamderax@gmail.com>
2025-11-05 11:39:19 -0800

    moving to Swift 6.2.1 and renaming artifact from edgeos to wendyos

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,18 +2,18 @@
 set -e
 
 # Variables
-SWIFT_PKG_URL="https://download.swift.org/swift-6.1-release/xcode/swift-6.1-RELEASE/swift-6.1-RELEASE-osx.pkg"
-WENDY_SDK_URL="https://github.com/wendylabsinc/wendy-swift-tools/releases/download/v0.1-pre-release/6.1-RELEASE_edgeos_aarch64.artifactbundle.tar.gz"
+SWIFT_PKG_URL="https://download.swift.org/swift-6.2.1-release/xcode/swift-6.2.1-RELEASE/swift-6.2.1-RELEASE-osx.pkg"
+WENDY_SDK_URL="https://github.com/wendylabsinc/wendy-swift-tools/releases/download/v0.2-pre-release/6.2.1-RELEASE_wendyos_aarch64.artifactbundle.tar.gz"
 
 SWIFT_PKG_FILE="$(basename $SWIFT_PKG_URL)"
 WENDY_SDK_FILE="$(basename $WENDY_SDK_URL)"
-WENDY_SDK_NAME="6.1-RELEASE_edgeos_aarch64"
+WENDY_SDK_NAME="6.2.1-RELEASE_wendyos_aarch64"
 
 # Install Swift Toolchain
-echo "Downloading Swift 6.1..."
+echo "Downloading Swift 6.2.1..."
 curl -LO "$SWIFT_PKG_URL"
 
-echo "Installing Swift 6.1 toolchain... "
+echo "Installing Swift 6.2.1 toolchain... "
 sudo installer -pkg "$SWIFT_PKG_FILE" -target /
 
 # Download custom Wendy SDK
